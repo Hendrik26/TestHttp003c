278764d28e67f17ca23f1f7934ecb8eb25aae0ec
Hendrik <hendrik.herdler@gmail.com>
2018-10-23 15:53:26 +0200

    added database connection - debugger configuration

--- src/php/write_json_db.php
+++ src/php/write_json_db.php
@@ -2,6 +2,21 @@
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+}
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
+}
 $json = file_get_contents("php://input");
 $file = fopen('json.txt', 'w+');
 fwrite($file, $json);
