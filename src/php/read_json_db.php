05db223ab3d55db345c94cf26c777868687cb004
Hendrik <hendrik.herdler@gmail.com>
2018-10-24 07:56:08 +0200

    added several improvements

--- src/php/read_json_db.php
+++ src/php/read_json_db.php
@@ -7,19 +7,11 @@ $dbname = "test";
 $sql = "select column01, column02 from tbltesthttp";
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $result = $conn->query($sql);
-    $resultArray = array();
-    if($result->rowCount() > 0) {
-        $rows = $result->fetchAll();
-        foreach ($rows as $row) {
-
-            $X = $row['column01'];
-            array_push($resultArray, array("column01"=>$row['column01'], "column02"=>$row['column02']));
-        };
-        $json = json_encode($resultArray);
-    }
+    $result->setFetchMode(PDO::FETCH_ASSOC);
+    $rows = $result->fetchAll();
+    $json = json_encode($rows);
     $conn = null;
 } catch (PDOException $e) {
     echo "read table failed: " . $e->getMessage();
