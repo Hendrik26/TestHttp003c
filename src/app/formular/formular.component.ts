627f1f02cd89e239f10563e269a72258b18774fb
Hendrik <hendrik.herdler@gmail.com>
2019-09-14 21:41:06 +0200

    Fix error with import of rxjs-library.

--- src/app/formular/formular.component.ts
+++ src/app/formular/formular.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {DemosrvService} from '../demosrv.service';
-import {Observable} from 'rxjs/Rx';
+// import {Observable} from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 import {Row} from '../row';
 
 @Component({
